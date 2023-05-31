69bc4b53aa88a99b879e31e919ed1f4a1055d858
aa.kruglova <ko-alla@inbox.ru>
2023-05-31 21:46:22 +0300

    comment

--- Homework7/task3.py
+++ Homework7/task3.py
@@ -1,3 +1,5 @@
+
+
 class PublicTransport:
     def __init__(self, brand, engine_power, year, color, max_speed):
         self.brand = brand                 # 1. Марка
@@ -14,34 +16,63 @@ class PublicTransport:
 
 
 class Bus(PublicTransport):
-    def __init__(self, brand, engine_power, year, color, max_speed, passengers, park, fare):
-        self.brand = brand
-        self._engine_power = engine_power
-        self.year = year
-        self.color = color
-        self.max_speed = max_speed
+    def __init__(self, brand, engine_power, year, color, max_speed, passengers, park: tuple, fare):
+        super().__init__(brand, engine_power, year, color, max_speed)
         self.passengers = passengers
         self.__park = park
         self._fare = fare
 
-    @property
-    def park(self):
-        if (int(self.__park) < 9999 and int(self.__park) > 1000):
-            return int(self.park)
 
+    @property
+
+    def park(self):
+            if self.__park in [1000, 9999]:
+                return self.__park
+            else:
+                return False
 
 class Tram(PublicTransport):
     def __init__(self, brand, engine_power, year, color, max_speed, route, path, fare):
-        self.brand = brand
-        self._engine_power = engine_power
-        self.year = year
-        self.color = color
-        self.max_speed = max_speed
+        super().__init__(brand, engine_power, year, color, max_speed)
         self.__route = route
         self.path = path
         self._fare = fare
+
 
     @property
     def how_long(self):
         time = self.max_speed / (4 * self.path)
         return time
+
+
+transport = PublicTransport('Автомобиль', 500, 2040, 'Фиолетовый', 300)
+first_bus = Bus('ЛиАЗ', 210, 2015, 'Зеленый', 100, 70, 1232, 32)
+second_bus = Bus('VOLGABUS', 320, 2019, 'Желтый', 110, 39, 1111, 32)
+first_tram = Tram('71-931M', 125, 2010, 'Красный', 75, 5, 15, 32)
+second_tram = Tram('71-409-1', 240, 2018, 'Белый', 85, 7, 17, 32)
+
+assert isinstance(type(transport).info, property), 'В классе PublicTransport, info - не свойство класса'
+assert transport._engine_power, 'В классе PublicTransport, engine_power не защищенный атрибут'
+assert first_bus._Bus__park, 'В классе Bus, park не приватный атрибут'
+assert second_bus._fare, 'В классе Bus, fare не защищенный атрибут'
+assert first_tram._fare, 'В классе Tram, fare не защищенный атрибут'
+assert second_tram._Tram__route, 'В классе Tram, route не приватный атрибут'
+assert isinstance(type(first_tram).how_long, property), 'В классе Tram, how_long - не свойство класса'
+assert first_tram.how_long == 1.25, 'В классе Tram, how_long неверно вычисляется'
+assert isinstance(type(second_bus).park, property), 'В классе Bus, park - не свойство класса'
+try:
+    second_bus.park = 999
+    raise Exception('Проверка на ограничение диапазона НЕ пройдена')
+except AssertionError:
+    print('Проверка на правильность диапазона пройдена')
+try:
+    second_bus.park = 1234
+    print('Проверка на правильность диапазона пройдена')
+except AssertionError:
+    raise Exception('Проверка на ограничение диапазона НЕ пройдена')
+try:
+    second_bus.park = 10000
+    raise Exception('Проверка на ограничение диапазона НЕ пройдена')
+except AssertionError:
+    print('Проверка на правильность диапазона пройдена')
+print('Всё ок')
